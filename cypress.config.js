e2a53c322f0cec853331cc2a4de2fb04aa4f8379
David Souto <csouto.david@gmail.com>
2025-06-11 21:56:34 -0300

    Configure cypress-split

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,15 @@
 const { defineConfig } = require("cypress")
+const cypressSplit = require("cypress-split")
 
 module.exports = defineConfig({
   viewportHeight: 1024,
   viewportWidth: 1700,
   e2e: {
     fixturesFolder: false, // indicates to Cypress that folder fixture will be not used.
+    setupNodeEvents(on, config) {
+      cypressSplit(on, config)
+      return config
+    }
   },
   defaultCommandTimeout: 6000
 })
